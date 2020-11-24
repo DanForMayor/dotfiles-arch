4a9fb7897c120807b3ca0dab34f7fded672a1d13
DanForMayor <danmccann0@gmail.com>
2020-11-24 14:16:24 -0700

    Updates

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -10,12 +10,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+bar="top" # This whole change here is hacky and disgusting
+
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     export MONITOR=$m
-	polybar --reload top &
-    polybar --reload bottom &
+
+	polybar --reload "$bar"&
+	bar="bottom"
   done
 else
   polybar --reload example &
