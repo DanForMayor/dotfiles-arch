681c8c6757acba86f24871bde380e70282c73ac4
DanForMayor <danmccann0@gmail.com>
2021-05-20 17:30:51 -0600

    Added screen recording script

---
+++ scripts/recordScreen.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# INFO ON PULSE STUFF RETRIEVED FROM THIS ARTICLE
+# https://www.maartenbaert.be/simplescreenrecorder/recording-game-audio/
+
+# Get hacksaw output
+hck="$(hacksaw)"
+
+# split size and offset
+size="$(echo "$hck" | grep -oP "\d+x\d+")"
+offset="$(echo "$hck" | grep -oP "\+\d+\+\d+")"
+
+# Can't get offX without the + without pulling out some more regex trash
+offX="$(echo "$offset" | grep -oP "^\+\d+")"
+offX="${offX:1}"
+
+# offY comes 
+offY="$(echo "$offset" | grep -oP "\d+$")"
+
+if [[ $hck = "" ]]; then
+	exit 1
+fi
+
+if [[ $1 = "all" ]]; then
+	# APPLICATION/NOTHING AND MIC
+	ffmpeg \
+		-video_size $size \
+		-framerate 60 \
+		-f x11grab \
+		-i :0.0+$offX,$offY \
+		-f pulse -ac 2 -i duplex_out.monitor output.webm
+
+elif [[ $1 = "nomic" ]]; then
+	# ONLY APPLICATIONS OR NOTHING
+	ffmpeg \
+		-video_size $size \
+		-framerate 60 \
+		-f x11grab \
+		-i :0.0+$offX,$offY \
+		-f pulse -ac 2 -i application_out.monitor output.webm
+
+else
+	echo "invalid mode (all/nomic)"
+fi
+
